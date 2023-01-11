9ccb1c591cacf4e0a2fb9238b4505590dc6dabdf
Ryo_gk <ryo.gk55@gmail.com>
2023-01-12 01:37:52 +0900

    init

---
+++ dev/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import LineBreak from '../lib/LineBreak.vue'
+
+const text = `どうも、初めまして。田中と申します。本日は簡単に適切な位置で改行を入れる方法を説明したいと思います。`
+</script>
+
+<template>
+  <div style="width: 500px;">
+    <LineBreak :text="text" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ lib/LineBreak.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { loadDefaultJapaneseParser } from 'budoux';
+
+const props = defineProps<{
+  text: string
+}>()
+
+const parser = loadDefaultJapaneseParser();
+const contents = parser.parse(props.text).join('<wbr>')
+
+console.log(contents)
+</script>
+
+<template>
+  <div class="LineBreak" v-html="contents">
+  </div>
+</template>
+
+<style scoped>
+.LineBreak {
+  word-break: keep-all;
+  overflow-wrap: break-word;
+}
+
+</style>
